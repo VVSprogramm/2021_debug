d1b98cd05dfa60b9fffb7b730c737e4385116b1a
VVSprogramm <vvsprogrammesana@gmail.com>
2021-09-21 15:37:55 +0300

    3uzd

---
+++ 3uzd.py
@@ -0,0 +1,13 @@
+#Lietotājs ievada veselu pozitīvu skaitli. 
+# Izdrukā visus skaitļus sākot no ievadītā līdz 0 (neieskaitot)
+
+#input
+#while cikls
+# >
+# -=
+
+sk = int(input("Ievadi veselu skaitli: "))
+
+while sk>0:
+    print(sk)
+    sk -= 1
